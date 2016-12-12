b3da641b2eaf6bda7ba743d09a9ff3fc99bab1d9
damon_lin <825348451@qq.com>
2016-12-12 21:40:14 +0800

    rename file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     author='Damon Lin',
     author_email='linshengsheng@growingio.com',
-    url='http://www.growingio.com',
+    url='https://github.com/growingio/growingio-python-sdk',
     license='LICENSE.txt',
     packages=['growingio'],
     description='This is the official Python SDK for GrowingIO.',
